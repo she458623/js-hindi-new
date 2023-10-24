ff4b2838b2273fcf995502c2e0e3a1fda369aaef
she458623 <102521296+she458623@users.noreply.github.com>
2023-10-24 10:16:30 +0000

    2nd day

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,23 @@
+
+// data type
+"use strict"; // treate all js code as a newer version
+let name = "shekhar"
+let isLoggedIn = True 
+
+
+// alert ( 3 + 3) we are using nodejs not browser
+ // string
+ // number 2 power 53 range biginit for big numbers
+ // boolean true or false
+ // null = standalone value
+ // undefined
+ // symbol = foruniqueness
+
+
+
+
+ //object
+ console.log(typeof age);
+ console.log(typeof null);
+
+ 
