bb5e839514b5dbb3a775d0f6b0af7cb251b4148a
she458623 <102521296+she458623@users.noreply.github.com>
2023-10-24 19:29:43 +0000

    string conversion

---
+++ 01_basic/05_string.js
@@ -0,0 +1,20 @@
+ const name = "Shekhar"
+ const repoCount = 50;
+
+//  e.loconsole.log(name + repoCount + "Tripathi");//string + string
+//  consolg(`hello my name is ${name} and my repo count is ${repoCount}`); //standard type 
+
+ const gameName = new String('Shekhar-De  ')
+
+//  console.log(gameName[0]);//position
+//  console.log(gameName.__proto__);//object
+//  console.log(gameName.length);//length of string 
+//  console.log(gameName.toLocaleUpperCase());// string change in upper case
+//  console.log(gameName.charAt(5)); // char value 
+//  console.log(gameName.indexOf("a"));// index value 
+
+const newString = gameName.substring(0,5);
+console.log(newString);//for substring 
+
+const anotherString = gameName.slice(-5, 5);
+
