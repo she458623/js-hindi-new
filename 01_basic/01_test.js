e4f5c5517418ab7a0f82d42cc5913a84609fbe1e
she458623 <102521296+she458623@users.noreply.github.com>
2023-10-24 10:15:05 +0000

    Conversion Of data Type

---
+++ 01_basic/01_test.js
@@ -0,0 +1,41 @@
+const accountId = 144553
+let accountEmail = "she@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+
+accountEmail = "shekhar@gmail.com"
+accountPassword = "123456"
+accountCity = "Raipur"
+accountState;
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
+console.log(accountId);
+console.log(typeof age);
+console.log(typeof null);
+
+
+// data type
+"use strict"; // treate all js code as a newer version
+let name = "shekhar"
+let isLoggedIn = True 
+
+
+// alert ( 3 + 3) we are using nodejs not browser
+ // string
+ // number 2 power 53 range biginit for big numbers
+ // boolean true or false
+ // null = standalone value
+ // undefined
+ // symbol = foruniqueness
+
+
+
+
+ //object
+ console.log(typeof age);
+ console.log(typeof null);
+
+ 
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "Shekhar"
+
+
+let valueInNumbers = Number(score);
+console.log(valueInNumbers);
+
+
+
+console.log(typeof valueInNumbers);
+console.log(typeof score);
+console.log(typeof (score));
+
+
+// 33 = 33
+// "33abc" = NaN
+// true = 1 or false = 0
+//"Shekhar" = NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+ //if 1 = true
+ // if 0 = false
+ //"" = false
+ //"shekhar" = true
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
